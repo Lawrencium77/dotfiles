f7fbd159c600fa62608dd2a5bad8358a2b8e7e82
Lawrence Atkins <lawrencea@speechmatics.com>
2024-06-08 15:59:27 +0100

    remove neovim install scripts

--- install_scripts/util.sh
+++
@@ -1,5 +0,0 @@
-# This is where install_scripts install binaries
-# Exposed here so they can pick it up and so zshrc can pick it up 
-# to add to path
-MY_BIN_LOC="$HOME/.local/mybins"
-mkdir -p $MY_BIN_LOC
